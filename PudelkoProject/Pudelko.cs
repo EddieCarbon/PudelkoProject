161c334d1c1cd1d1c5b5f8f89eedc9f4cf2178ca
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-03-28 16:28:59 +0200

    Add Constructor

--- PudelkoProject/Pudelko.cs
+++ PudelkoProject/Pudelko.cs
@@ -23,9 +23,24 @@ namespace PudelkoProject
             else if (unit == UnitOfMeasure.milimeter) factor = 1;
 
             double[] dimensions = new double[3];
+            if (a is not null) dimensions[0] = Math.Floor((double)a * factor);
+            else dimensions[0] = 100;
+            if (b is not null) dimensions[1] = Math.Floor((double)b * factor);
+            else dimensions[1] = 100;
+            if (c is not null) dimensions[2] = Math.Floor((double)c * factor);
+            else dimensions[2] = 100;
+
+            for (int i = 0; i < dimensions.Length; i++)
+                if (dimensions[i] > 10000 | dimensions[i] < 1)
+                    throw new ArgumentOutOfRangeException("Wrong dimension!");
+
+            this.dimensions = dimensions;
+        } // Main constructor
 
 
-        }
+
+
+    }
 
 
         #endregion
