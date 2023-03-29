31a49d78c0533beb1f5a5c5032d43e71685b0a9a
jakubtomiczek <jakub.tomiczek@microsoft.wsei.edu.pl>
2023-03-29 22:44:29 +0200

    KompresujMethod changed

--- PudelkoProject/KompresujMethod.cs
+++ PudelkoProject/KompresujMethod.cs
@@ -6,7 +6,7 @@ namespace PudelkoProject
     {
         public static Pudelko Kompresuj(this Pudelko p)
         {
-            if (p.A == p.B & p.A == p.C)
+            if ((p.A == p.B) & (p.A == p.C))
                 return new Pudelko(p.A, p.B, p.C);
 
             double edge = Math.Round(Math.Pow(p.Objetosc, (double)1 / 3), 3);
